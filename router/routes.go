592bdd66fb0d0bdf7947e92ef7bfb930eadc8969
Daniel Gabriel <reelpscanal@gmail.com>
2023-04-25 14:30:16 -0300

    update: routes + swagger

--- router/routes.go
+++ router/routes.go
@@ -3,17 +3,25 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/revogabe/go-jobsdev/handler"
+
+	docs "github.com/revogabe/go-jobsdev/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Initialize handler
 	handler.InitializeHandler()
-	v1 := router.Group("/api/v1") 
+	basePath := "/api/v1"
+
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath) 
 	{
 		v1.GET("/jobs", handler.ShowJobsHandler)
 		v1.POST("/create", handler.CreateJobsHandler)
 		v1.DELETE("/delete", handler.DeleteJobsHandler)
-		v1.PUT("/update", handler.UpdateJobsHandler)
 		v1.GET("/jobsall", handler.ListJobsHandler)
 	}
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
