227105313e2637285a1e6879759b41baeb375391
Daniel Gabriel <reelpscanal@gmail.com>
2023-04-25 17:12:54 -0300

    fix approved

--- handler/approvedJobs.go
+++ handler/approvedJobs.go
@@ -14,8 +14,6 @@ import (
 func ApprovedJobsHandler(ctx *gin.Context) {
 	request := ApprovedJobsRequest{}
 
-	ctx.BindJSON(&request)
-
 	err := godotenv.Load()
   if err != nil {
     log.Fatal("Error loading .env file")
