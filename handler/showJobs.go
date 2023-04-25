193e13d85978050a77a5fd3eb1e728195ed44c5f
Daniel Gabriel <reelpscanal@gmail.com>
2023-04-25 14:30:07 -0300

    feat: add handler showJobs

--- handler/showJobs.go
+++ handler/showJobs.go
@@ -2,11 +2,24 @@ package handler
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/revogabe/go-jobsdev/schemas"
 )
 
 func ShowJobsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H {
-		"message": "GET jobs!",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	jobs := schemas.Jobs{}
+
+	if err := db.First(&jobs, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, err.Error())
+		return
+	}
+
+	sendSuccess(ctx, "show-job", jobs)
 }
