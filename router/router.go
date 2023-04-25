9073b195a76ba23efaa9720084f8b31f4c48d689
Daniel Gabriel <56942108+revogabe@users.noreply.github.com>
2023-04-25 16:08:23 -0300

    Update router.go

--- router/router.go
+++ router/router.go
@@ -5,7 +5,12 @@ import "github.com/gin-gonic/gin"
 func Initialize() {
 	router := gin.Default()
 
-	initializeRoutes(router)
-	
-	router.Run(":8080") // 
+	// Get the port from the environment
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Run the server
+	router.Run("0.0.0.0:" + port)
 }
