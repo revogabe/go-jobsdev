1c0a876cf80c1a058cc51818ec1659e7e2cfea39
Daniel Gabriel <reelpscanal@gmail.com>
2023-04-25 14:29:08 -0300

    feat: add handler delete

--- handler/deleteJobs.go
+++ handler/deleteJobs.go
@@ -1,12 +1,31 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/revogabe/go-jobsdev/schemas"
 )
 
 func DeleteJobsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H {
-		"message": "DELETE jobs!",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	jobs := schemas.Jobs{}
+
+	if err := db.Find(&jobs, id).Error; err != nil {
+		sendError(ctx, http.StatusFound, fmt.Sprintf("Error finding jobs with id %s on database", id))
+		return
+	}
+
+	if err := db.Delete(&jobs).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("Error deleting jobs with id %s on database", id))
+		return
+	}
+
+	sendSuccess(ctx, "delete-jobs", jobs)
 }
