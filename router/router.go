90a12c4d6b5343124ae04bef5b73871c50dcd6db
Daniel Gabriel <reelpscanal@gmail.com>
2023-04-25 21:11:10 -0300

    fix: cors router

--- router/router.go
+++ router/router.go
@@ -3,12 +3,18 @@ package router
 import (
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Initialize() {
 	// Initialize Router
 	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	router.Use(cors.New(config))
+
 
 	// Initialize Routes
 	initializeRoutes(router)
