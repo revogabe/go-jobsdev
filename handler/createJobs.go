ff7588dc11fadd2a06a75c03ba95f6a6b84ac1cd
ArielBetti <ariel.betti@gmail.com>
2023-04-26 14:32:48 -0300

    fix: return schema inferation id

--- handler/createJobs.go
+++ handler/createJobs.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/revogabe/go-jobsdev/schemas"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func CreateJobsHandler(ctx *gin.Context) {
@@ -23,13 +24,34 @@ func CreateJobsHandler(ctx *gin.Context) {
 		return
 	}
 
-	var jobs schemas.Jobs
+	// Criar uma instância vazia da estrutura Jobs
+	jobs := schemas.Jobs{}
 
-	if _, err := db.Collection("jobs").InsertOne(context.Background(), jobs); err != nil {
+	// Definir valores dos campos da estrutura Jobs com base no objeto request
+	jobs.Title = request.Title
+	jobs.Description = request.Description
+	jobs.Role = request.Role
+	jobs.Company = request.Company
+	jobs.Location = request.Location
+	jobs.Remote = request.Remote
+	jobs.Link = request.Link
+	jobs.Experience = request.Experience
+	jobs.Salary = request.Salary
+	jobs.Approved = false
+
+	// Inserir jobs no banco de dados
+	res, err := db.Collection("jobs").InsertOne(context.Background(), jobs)
+	if err != nil {
 		logger.Errorf("Error creating jobs: %v", err.Error())
 		sendError(ctx, http.StatusInternalServerError, "Error creating jobs on database")
 		return
 	}
+
+	// Extrair valor do atributo _id da resposta da função InsertOne
+	id := res.InsertedID.(primitive.ObjectID)
+
+	// Definir valor do campo _id da estrutura Jobs com o valor do atributo _id
+	jobs.ID = id
 
 	sendSuccess(ctx, "create-jobs", jobs)
 }
